83a74597098ca69f9f0024bd360f137e1e1ef304
bistok diiel lelono <bistok@gmail.com>
2021-02-23 09:00:13 +0700

    init AddUserInbox

--- internal/usecase/add.user.inbox.go
+++ internal/usecase/add.user.inbox.go
@@ -7,3 +7,11 @@ import (
 )
 
 type AddUserInbox func(ctx context.Context, userID string) (resp *model.UserInboxConfig, err error)
+
+func NewUserInboxAdder() AddUserInbox {
+	return func(ctx context.Context, userID string) (resp *model.UserInboxConfig, err error) {
+		// generate random inbox name, user/password
+		// then store to db
+		return
+	}
+}
